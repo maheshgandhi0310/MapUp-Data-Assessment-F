93086c972cf0a93402e27ec38fd45a759ec2ab3f
maheshgandhi0310 <84694316+maheshgandhi0310@users.noreply.github.com>
2023-12-10 10:30:13 +0530

    commit1
    commit1

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -13,8 +13,11 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
-
-    return df
+    matrix = df.pivot(index='id_1', columns='id_2', values='car')
+    matrix = matrix.fillna(0)
+    for col in matrix.columns:
+        matrix.loc[col, col] = 0
+    return matrix
 
 
 def get_type_count(df)->dict:
@@ -28,8 +31,10 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
-
-    return dict()
+    df['car_type'] = pd.cut(df['car'], bins=[-float('inf'), 15, 25, float('inf')], labels=['low', 'medium', 'high'])
+    type_counts = df['car_type'].value_counts().to_dict()
+    sorted_type_counts = dict(sorted(type_counts.items()))
+    return sorted_type_counts
 
 
 def get_bus_indexes(df)->list:
@@ -43,8 +48,10 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
-
-    return list()
+    mean_bus = df['bus'].mean()
+    bus_indexes = df[df['bus'] > 2 * mean_bus].index.tolist()
+    bus_indexes.sort()
+    return bus_indexes
 
 
 def filter_routes(df)->list:
@@ -58,8 +65,10 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
-
-    return list()
+    avg_truck = df.groupby('route')['truck'].mean()
+    filtered_routes = avg_truck[avg_truck > 7].index.tolist()
+    filtered_routes.sort()
+    return filtered_routes
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -73,8 +82,10 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
-
-    return matrix
+    modified_matrix = matrix.copy()
+    modified_matrix = modified_matrix.applymap(lambda x: x * 0.75 if x > 20 else x * 1.25)
+    modified_matrix = modified_matrix.round(1)
+    return modified_matrix
 
 
 def time_check(df)->pd.Series:
@@ -88,5 +99,36 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    df['start_timestamp'] = pd.to_datetime(df['startDay'] + ' ' + df['startTime'])
+    df['end_timestamp'] = pd.to_datetime(df['endDay'] + ' ' + df['endTime'])
+    incorrect_timestamps = (df['end_timestamp'] - df['start_timestamp'] != pd.Timedelta(days=1)) | (df['end_timestamp'].dt.dayofweek - df['start_timestamp'].dt.dayofweek != 6)
+    incorrect_timestamps = incorrect_timestamps.groupby([df['id'], df['id_2']]).any()
+    return incorrect_timestamps
 
-    return pd.Series()
+data_task1 = pd.read_csv('dataset-1.csv')
+data_task2 = pd.read_csv('dataset-2.csv')
+
+result_car_matrix = generate_car_matrix(data_task1)
+result_type_count = get_type_count(data_task1)
+result_bus_indexes = get_bus_indexes(data_task1)
+result_filtered_routes = filter_routes(data_task1)
+result_modified_matrix = multiply_matrix(result_car_matrix)
+result_time_check = time_check(data_task2)
+
+print("Result Car Matrix:")
+print(result_car_matrix)
+
+print("\nResult Car Type Count:")
+print(result_type_count)
+
+print("\nResult Bus Count Indexes:")
+print(result_bus_indexes)
+
+print("\nResult Filtered Routes:")
+print(result_filtered_routes)
+
+print("\nResult Modified Matrix:")
+print(result_modified_matrix)
+
+print("\nResult Time Check:")
+print(result_time_check)
--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -12,8 +12,20 @@ def calculate_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Distance matrix
     """
     # Write your logic here
-
-    return df
+    distance_matrix = df.pivot(index='id_start', columns='id_end', values='distance')
+    distance_matrix = distance_matrix.fillna(0)
+    distance_matrix = distance_matrix.add(distance_matrix.T, fill_value=0)
+    
+    for col in distance_matrix.columns:
+        for row in distance_matrix.index:
+            if row != col:
+                for via in distance_matrix.index:
+                    if via != col and via != row:
+                        if distance_matrix.at[row, via] != 0 and distance_matrix.at[via, col] != 0:
+                            if distance_matrix.at[row, col] == 0 or distance_matrix.at[row, col] > distance_matrix.at[row, via] + distance_matrix.at[via, col]:
+                                distance_matrix.at[row, col] = distance_matrix.at[row, via] + distance_matrix.at[via, col]
+    
+    return distance_matrix
 
 
 def unroll_distance_matrix(df)->pd.DataFrame():
@@ -27,8 +39,11 @@ def unroll_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
     # Write your logic here
-
-    return df
+    unrolled_matrix = matrix.stack().reset_index()
+    unrolled_matrix.columns = ['id_start', 'id_end', 'distance']
+    unrolled_matrix = unrolled_matrix[unrolled_matrix['id_start'] != unrolled_matrix['id_end']]
+    
+    return unrolled_matrix
 
 
 def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
@@ -44,8 +59,13 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
                           of the reference ID's average distance.
     """
     # Write your logic here
-
-    return df
+    avg_distance = df[df['id_start'] == reference_value]['distance'].mean()
+    lower_bound = avg_distance * 0.9
+    upper_bound = avg_distance * 1.1
+    within_threshold = df[(df['id_start'] != reference_value) & (df['distance'] >= lower_bound) & (df['distance'] <= upper_bound)]['id_start'].unique()
+    within_threshold = sorted(within_threshold)
+    
+    return within_threshold
 
 
 def calculate_toll_rate(df)->pd.DataFrame():
@@ -59,7 +79,11 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
-
+    rate_coefficients = {'moto': 0.8, 'car': 1.2, 'rv': 1.5, 'bus': 2.2, 'truck': 3.6}
+    
+    for vehicle in rate_coefficients:
+        df[vehicle] = df['distance'] * rate_coefficients[vehicle]
+    
     return df
 
 
@@ -74,5 +98,58 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
+    days_mapping = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+    
+    df['start_day'] = df['start_timestamp'].dt.dayofweek.map(days_mapping)
+    df['end_day'] = df['end_timestamp'].dt.dayofweek.map(days_mapping)
+    
+    weekday_discounts = {
+        (time(0, 0, 0), time(10, 0, 0)): 0.8,
+        (time(10, 0, 0), time(18, 0, 0)): 1.2,
+        (time(18, 0, 0), time(23, 59, 59)): 0.8
+    }
+    weekend_discount = 0.7
+    
+    def calculate_rate(row):
+        if row['start_day'] in ['Saturday', 'Sunday']:
+            return weekend_discount
+        else:
+            for time_range, discount_factor in weekday_discounts.items():
+                start_time, end_time = time_range
+                if start_time <= row['start_timestamp'].time() <= end_time:
+                    return discount_factor
+            return 1.0
+    
+    for vehicle in ['moto', 'car', 'rv', 'bus', 'truck']:
+        df[vehicle] = df.apply(lambda row: row[vehicle] * calculate_rate(row), axis=1)
+    
+    df['start_time'] = df['start_timestamp'].dt.time
+    df['end_time'] = df['end_timestamp'].dt.time
+    df.drop(['start_timestamp', 'end_timestamp'], axis=1, inplace=True)
 
     return df
+
+
+data_task2 = pd.read_csv('dataset-3.csv')
+
+result_distance_matrix = calculate_distance_matrix(data_task2)
+result_unrolled_matrix = unroll_distance_matrix(result_distance_matrix)
+reference_id = 1  # Set your reference ID here
+result_within_threshold = find_ids_within_ten_percentage_threshold(result_unrolled_matrix, reference_id)
+result_toll_rates = calculate_toll_rate(result_unrolled_matrix)
+result_time_based_toll_rates = calculate_time_based_toll_rates(result_within_threshold)
+
+print("Result Distance Matrix:")
+print(result_distance_matrix)
+
+print("\nResult Unrolled Distance Matrix:")
+print(result_unrolled_matrix)
+
+print("\nResult IDs within Ten Percentage Threshold:")
+print(result_within_threshold)
+
+print("\nResult Toll Rates:")
+print(result_toll_rates)
+
+print("\nResult Time-Based Toll Rates:")
+print(result_time_based_toll_rates)
